a2dc889824a9cae0e3e2bc495249f75e5fabe939
Denis <fokin_denis@mail.ru>
2013-03-14 23:29:28 +0400

    IM not finished

--- GPSLocalService/src/com/OsMoDroid/IM.java
+++ GPSLocalService/src/com/OsMoDroid/IM.java
@@ -1,5 +1,5 @@
 
-package com.OsMoDroid;import java.io.BufferedReader;import java.io.IOException;import java.io.InputStream;import java.io.InputStreamReader;import java.io.PrintWriter;import java.net.HttpURLConnection;import java.net.InetSocketAddress;import java.net.Proxy;import java.net.URL;import java.net.UnknownHostException;import java.text.SimpleDateFormat;import java.util.ArrayList;import java.util.Date;import java.util.Iterator;import org.json.JSONArray;import org.json.JSONException;import org.json.JSONObject;import com.OsMoDroid.LocalService.SendCoor;import android.app.Notification;import android.app.PendingIntent;import android.app.PendingIntent.CanceledException;import android.content.BroadcastReceiver;import android.content.Context;import android.content.Intent;import android.content.IntentFilter;import android.net.NetworkInfo;import android.os.Bundle;import android.os.Handler;import android.os.Message;import android.support.v4.app.NotificationCompat;import android.util.Log;import android.widget.Toast;public class IM {	protected boolean running       = false;	protected boolean connected     = false;	protected boolean autoReconnect = true;	protected Integer timeout       = 0;	private HttpURLConnection con;	private InputStream instream;	String adr;	String mykey;	String timestamp=Long.toString(System.currentTimeMillis());	int pingTimeout=900;	Thread myThread;	private BufferedReader    in      = null;	Context parent;	String myLongPollCh;
+package com.OsMoDroid;import java.io.BufferedReader;import java.io.IOException;import java.io.InputStream;import java.io.InputStreamReader;import java.io.PrintWriter;import java.net.HttpURLConnection;import java.net.InetSocketAddress;import java.net.Proxy;import java.net.URL;import java.net.UnknownHostException;import java.text.SimpleDateFormat;import java.util.ArrayList;import java.util.Date;import java.util.Iterator;import org.json.JSONArray;import org.json.JSONException;import org.json.JSONObject;import com.OsMoDroid.LocalService.SendCoor;import android.app.Notification;import android.app.PendingIntent;import android.app.PendingIntent.CanceledException;import android.content.BroadcastReceiver;import android.content.Context;import android.content.Intent;import android.content.IntentFilter;import android.net.NetworkInfo;import android.os.Bundle;import android.os.Handler;import android.os.Message;import android.support.v4.app.NotificationCompat;import android.util.Log;import android.widget.Toast;public class IM {	protected  boolean running       = false;	protected boolean connected     = false;	protected boolean autoReconnect = true;	protected Integer timeout       = 0;	private HttpURLConnection con;	private InputStream instream;	String adr;	String mykey;	String timestamp=Long.toString(System.currentTimeMillis());	int pingTimeout=900;	Thread myThread;	private BufferedReader    in      = null;	Context parent;	String myLongPollCh;
 	ArrayList<String[]>  myLongPollChList;	//ArrayList<String> list= new ArrayList<String>();	int mestype=0;	final private static SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");	public IM(ArrayList<String[]> longPollChList, Context context,String key) {
 	
 		
@@ -22,12 +22,25 @@ package com.OsMoDroid;import java.io.BufferedReader;import java.io.IOExcep
 	}
 	
 	public void addchannels(ArrayList<String[]> longPollChList){
-		parent.unregisterReceiver(bcr);
-		stop();
+		//parent.unregisterReceiver(bcr);
+		//stop();
 		myLongPollChList.addAll(longPollChList);
 		getadres(myLongPollChList);
-		parent.registerReceiver(bcr, new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"));
-		start();
+		try {
+
+
+
+			con.disconnect();
+
+			} catch (Exception e) {
+
+
+
+				e.printStackTrace();
+
+			}
+		//parent.registerReceiver(bcr, new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"));
+		//start();
 		
 	}
 	
@@ -42,7 +55,7 @@ package com.OsMoDroid;import java.io.BufferedReader;import java.io.IOExcep
 	
 	return ids;
 	
-}			public void showToastMessage(String message) {			Message msg = new Message();			Bundle b = new Bundle();			b.putString("MessageText", message);			msg.setData(b);			LocalService.alertHandler.sendMessage(msg);			}	private BroadcastReceiver bcr = new BroadcastReceiver() {		@Override		public void onReceive(Context context, Intent intent) {			Log.d(this.getClass().getName(), "BCR"+this);			Log.d(this.getClass().getName(), "BCR"+this+" Intent:"+intent);			if (intent.getAction().equals(android.net.ConnectivityManager.CONNECTIVITY_ACTION)) {				Bundle extras = intent.getExtras();				Log.d(this.getClass().getName(), "BCR"+this+ " "+intent.getExtras());				if(extras.containsKey("networkInfo")) {					NetworkInfo netinfo = (NetworkInfo) extras.get("networkInfo");					Log.d(this.getClass().getName(), "BCR"+this+ " "+netinfo);					Log.d(this.getClass().getName(), "BCR"+this+ " "+netinfo.getType());					if(netinfo.isConnected()) {						Log.d(this.getClass().getName(), "BCR"+this+" Network is connected");						Log.d(this.getClass().getName(), "BCR"+this+" Running:"+running);						// Network is connected						if(!running ) {							System.out.println("Starting from Reciever"+this.toString());							myThread.interrupt();							start();						}					}					else {						System.out.println("Stoping1 from Reciever"+this.toString());						stop();					}				}				else if(extras.containsKey("noConnectivity")) {					System.out.println("Stoping2 from Reciever"+this.toString());					stop();				}		    }		}	};	void close(){		parent.unregisterReceiver(bcr);		stop();	};	void start(){		this.running = true;		System.out.println("About to notify state from start()");		System.out.println("State notifed of start()");		myThread = new Thread(new IMRunnable());		myThread.start();	}void parseEx (String toParse){
+}			public void showToastMessage(String message) {			Message msg = new Message();			Bundle b = new Bundle();			b.putString("MessageText", message);			msg.setData(b);			LocalService.alertHandler.sendMessage(msg);			}	private BroadcastReceiver bcr = new BroadcastReceiver() {		@Override		public void onReceive(Context context, Intent intent) {			Log.d(this.getClass().getName(), "BCR"+this);			Log.d(this.getClass().getName(), "BCR"+this+" Intent:"+intent);			if (intent.getAction().equals(android.net.ConnectivityManager.CONNECTIVITY_ACTION)) {				Bundle extras = intent.getExtras();				Log.d(this.getClass().getName(), "BCR"+this+ " "+intent.getExtras());				if(extras.containsKey("networkInfo")) {					NetworkInfo netinfo = (NetworkInfo) extras.get("networkInfo");					Log.d(this.getClass().getName(), "BCR"+this+ " "+netinfo);					Log.d(this.getClass().getName(), "BCR"+this+ " "+netinfo.getType());					if(netinfo.isConnected()) {						Log.d(this.getClass().getName(), "BCR"+this+" Network is connected");						Log.d(this.getClass().getName(), "BCR"+this+" Running:"+running);						// Network is connected						if(!running ) {							System.out.println("Starting from Reciever"+this.toString());							myThread.interrupt();							start();						}					}					else {						System.out.println("Stoping1 from Reciever"+this.toString());						stop();					}				}				else if(extras.containsKey("noConnectivity")) {					System.out.println("Stoping2 from Reciever"+this.toString());					stop();				}		    }		}	};	 void close(){		parent.unregisterReceiver(bcr);		stop();	};	 void start(){		this.running = true;		System.out.println("About to notify state from start()");		System.out.println("State notifed of start()");		myThread = new Thread(new IMRunnable());		myThread.start();	}void parseEx (String toParse){
 	
 	
 	try {
@@ -54,13 +67,15 @@ package com.OsMoDroid;import java.io.BufferedReader;import java.io.IOExcep
 		for (int i = 0; i < result.length(); i++) {
 
 		    JSONObject jsonObject = result.getJSONObject(i);
-		   
+		    Iterator it = (new JSONObject(jsonObject.optString("ids"))).keys();
+
+			  while (it.hasNext())
 		    
-		    while (jsonObject.keys().hasNext())
+		    
 
           	{
 		    	
-		    	String keyname= (String)jsonObject.keys().next();
+		    	String keyname= (String) it.next();
 		    	Log.d(this.getClass().getName(), "keyname="+keyname);
 		    	 timestamp = (new JSONObject(jsonObject.optString("ids"))).optString(keyname);
 		    	 Log.d(this.getClass().getName(), "timestamp="+ timestamp);
@@ -186,6 +201,35 @@ if (getMessageType( keyname).equals("ch")){
 if (getMessageType( keyname).equals("chch")){
 	Log.d(this.getClass().getName(), "type=chch");
 	Log.d(this.getClass().getName(), "Сообщение в чат канала " + jsonObject.optString("data"));
+	 //"data": "0|40|cxbcxvbcxvbcxvb|2013-03-14 22:42:34"
+	String[] data = jsonObject.optString("data").split("\\|");
+	Log.d(this.getClass().getName(), "data[0]=" + data[0] + " data[1]=" + data[1] + " data[2]=" + data[2]);
+	for (final Channel channel : LocalService.channelList) {
+		Log.d(this.getClass().getName(), "chanal nest" + channel.name);
+		for (Device device : channel.deviceList) {
+			Log.d(this.getClass().getName(), "device nest" + device.name + " " + device.u);
+			if (data[1].equals(device.u)) {
+				Log.d(this.getClass().getName(), "Сообщение от устройства в канале " + device.toString());
+				channel.messagesstringList.add(data[2]);
+				LocalService.alertHandler.post(new Runnable(){
+					public void run() {
+						if (LocalService.channelsmessagesAdapter!=null){
+							
+							
+							LocalService.channelsmessagesAdapter.notifyDataSetChanged();
+
+							
+						}
+					}
+				});
+				
+
+			}
+
+		}
+
+	}
+
 }
 
 
@@ -283,5 +327,6 @@ if (getMessageType( keyname).equals("chch")){
 			
 			
 			
-		}	}	void stop (){		try {			instream.close();			} catch (Exception e) {				e.printStackTrace();			}		try {			in.close();			} catch (Exception e) {				e.printStackTrace();			}		try {			con.disconnect();			} catch (Exception e) {				e.printStackTrace();			}			running = false;	}	private class IMRunnable implements Runnable {		StringBuilder stringBuilder= new StringBuilder();		private InputStreamReader stream  = null;		private boolean           error   = false;		private int               retries = 0;		int maxRetries = 100;		public void run() {			System.out.println("run thread instance:"+this.toString());			String response = "";			while (running) {
+		}	}	 void stop (){
+		running = false;		try {			instream.close();			} catch (Exception e) {				e.printStackTrace();			}		try {			in.close();			} catch (Exception e) {				e.printStackTrace();			}		try {			con.disconnect();			} catch (Exception e) {				e.printStackTrace();			}				}	private class IMRunnable implements Runnable {		StringBuilder stringBuilder= new StringBuilder(1024);		private InputStreamReader stream  = null;		private boolean           error   = false;		private int               retries = 0;		int maxRetries = 100;		public void run() {			System.out.println("run thread instance:"+this.toString());			String response = "";			while (running) {
 				Log.d(this.getClass().getName(), "adr="+adr);				System.out.println("running thread instance:"+this.toString());				try {					++retries;					int portOfProxy = android.net.Proxy.getDefaultPort();					if (portOfProxy > 0) {						Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(								android.net.Proxy.getDefaultHost(), portOfProxy));						con = (HttpURLConnection) new URL(adr).openConnection(proxy);					} else {						con = (HttpURLConnection) new URL(adr).openConnection();					}					con.setReadTimeout(pingTimeout*1000);					con.setConnectTimeout(10000);					instream=con.getInputStream();					stream = new InputStreamReader(instream);					in   = new BufferedReader(stream, 1024);					if(error){						error = false;					}					// Set a timeout on the socket					// This prevents getting stuck in readline() if the pipe breaks					retries = 0;					connected = true;				} catch (UnknownHostException e) {					error = true;					//stop();				} catch (Exception e) {					error = true;					//stop();				}				if(retries > maxRetries) {					stop ();					break;				}				if(retries > 0) {					try {						Thread.sleep(1000);					} catch (InterruptedException e) {						// If can't back-off, stop trying						System.out.println("Interrupted from sleep thread instance:"+this.toString());						//running = false;						break;					}				}				if(!error && running) {					try {						// Wait for a response from the channel						stringBuilder.setLength(0);						    int c = 0;						    int i=0;						    while (!(c==-1) && running) {						    	c = in.read();						        if (!(c==-1))stringBuilder.append((char) c);						        i=i+1;						    }					//return chbuf.toString();						    //Log.d(this.getClass().getName(), "void input end");						    parseEx( stringBuilder.toString());//						while(running && (response=in. .read()) != null) {//							// Got a response//							//killConnection.cancel();//							parse(response);//						}						instream.close();						in.close();						con.disconnect();					}					catch(Exception e) {						error = true;							//stop();						}					}				else {					// An error was encountered when trying to connect					connected = false;				}			}		}	}}
